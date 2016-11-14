6685fd55bfc81058a50fca15c593f82cc36c7dc5
MengLiMing <920459250@qq.com>
2016-11-14 16:00:08 +0800

    Initial Commit

---
+++ MLMSegmentPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLMSegmentPage
+//
+//  Created by my on 2016/11/14.
+//  Copyright © 2016年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLMSegmentPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLMSegmentPage
+//
+//  Created by my on 2016/11/14.
+//  Copyright © 2016年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
